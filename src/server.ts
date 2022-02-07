f39a1665694a6b66cd733cc9825595065dd1eb87
Thiago de Sa <wtss@protonmail.com>
2022-02-07 12:25:08 -0300

    Adicionado documentação do swagger

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,8 @@
 import { app } from "./index";
+import swaggerUi from "swagger-ui-express";
+
+import swaggerFile from "./swagger.json";
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.listen(3333, () => console.log("Server is running!"));
